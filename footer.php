e6cdf317365d92704083bdee9eb7187bdee3dddc
shephali97 <36363605+shephali97@users.noreply.github.com>
2018-04-08 00:23:30 +0530

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,80 +1,134 @@
-<htm>
-<head></head>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset=utf-8" />
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<body>
+<style>
+h3{
+	color:white;
+	font-size:1.5em;
+	margin-bottom:1em;
+	margin-left:1.4em;
+}
+ul {
+	list-style:none;
+	font-size:14px;
+	line-height:2.2em;
+color:#ABABAB;}
 
-<body>
-<!--footer-->
-<div class="footer">
-	<div class="container-fluid">
-		<div class="col-md-3 footer-grid">
-			<h3>About Us</h3>
+a{
+	color:white
+}
+
+a:hover{
+	background-color:white;	
+}
+
+	
+p{
+	font-size:14px;
+	line-height:2.2em;
+	color:white;
+	margin-left:2em;
+}
+	
+
+
+
+
+</style>
+</head>
+<body  class="container-fluid navbar navbar-inverse">
+<div class="row">
+
+<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 ">
+
+           <h3>About Us</h3>
 			<p>We provide the best functionality of buying grocery items online. Primary objective of this site is to facilitate the customer through online shopping so that customer will save a lot of time..</p>
-		</div>
-		<div class="col-md-3 footer-grid ">
-			<h3>Menu</h3>
-			<ul>
+</div>
+
+
+<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 ">
+
+            <h3>Menu</h3>
+			<ul >
 				<li><a href="index.html">Home</a></li>
-				<li><a href="">Cooking Essentials</a></li>
-				<li><a href="">Packaged Foods</a></li>
-				<li><a href="">Snacks</a></li>						 
-				<li><a href="">Beverages</a></li> 
-				<li><a href="">Household Supplies</a></li>
-	            <li><a href="">Haircare, Bath & Body</a></li>
-	            <li><a href="">Skincare & Make-up</a></li>
+				<li><a href="">Snacks</a></li>
+				<li><a href=""> Beverages</a></li>
+				<li><a href="">Packaged Foods</a></li>						 
+				<li><a href=""> Cooking Essentials</a></li> 
+				<li><a href=""> Skincare & Make-up </a></li>
+	            <li><a href="">Household Supplies</a></li>
+	            <li><a href="">Haircare, Bath & Body </a></li>
 				<li><a href="">Diapers, Baby Care & Food</a></li>
 				
 			</ul>
-		</div>
-		<div class="col-md-3 footer-grid ">
-			<h3>Customer Services</h3>
+ </div>
+ 
+ <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 ">
+ 
+            <h3>Customer Services</h3>
 			<ul>
 	
-				<li><a href="">Terms & Conditions</a></li>
-				<li><a href="">Faqs</a></li>
-				<li><a href="contactus.php">Contact Us</a></li>
+				<li><a href=""> Faqs </a></li>
+				<li><a href="">Contact Us</a></li>
+				<li><a href="contactus.php">Terms & Conditions  </a></li>
 										 
-				 
 			</ul>
-		</div>
-		<div class="col-md-3 footer-grid">
-			<h3>Payment Mode</h3>
-			<ul>
+ </div>
+ 
+ <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 ">
+ <h3>Payment Mode</h3 >
+            <ul >
 			    <li><img src="images/paytm.png"  width="50px">  <a href="">Paytm</a></li><br>
 				<li><img src="images/coddee.png" width="60px" height="40px" ><a href="" > Cash On Delivery</a></li><br>
 				<li><img src="images/debitcard.png" width="50px" height="40px"><a href="" >Debit/credit card</a></li>
 				
 				
 			</ul>
-		</div>
-		<div class="clearfix"></div>
-			<div class="footer-bottom">
-				<h2 ><a href="index.html"><b>T<br>H<br>E</b>Big Store<span>The Best Supermarket</span></a></h2>
-				<p class="fo-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
-				<ul class="social-fo">
-					<li><a href="#" class=" face"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-					<li><a href="#" class=" twi"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-					<li><a href="#" class=" pin"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-				</ul>
-				<div class=" address">
-					<div class="col-md-4 fo-grid1">
-							<p><i class="fa fa-home" aria-hidden="true"></i>12K Street , 45 Building Road Canada.</p>
-					</div>
-					<div class="col-md-4 fo-grid1">
-							<p><i class="fa fa-phone" aria-hidden="true"></i>+1234 758 839 , +1273 748 730</p>	
-					</div>
-					<div class="col-md-4 fo-grid1">
-						<p><a href="mailto:info@example.com"><i class="fa fa-envelope-o" aria-hidden="true"></i>info@example1.com</a></p>
-					</div>
-					<div class="clearfix"></div>
+ </div>
+ </div>
+ 
+ <div style="text-align:center;margin:50px;">
+ <h2 style="color:white;"><a href="index.html"><b>SHEZ KHAMP</a></h2>
+				<p style="padding: 1em 0 1.5em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
+				
+				<ul style="list-style:none;margin:2em;">
+					<li style="display:inline-block;"><a href="https://www.facebook.com/"> <img   src=images/fbicon.png width="60px" height="40px"></a></li>
+					<li style="display:inline-block;"><a href="#"><img   src=images/instagram.png width="60px" height="40px"></a></li>
+					<li style="display:inline-block;"><a href="#"><img   src=images/twitter.png width="60px" height="40px"></a></li>
 					
-					</div>
-			</div>
-		<div class="copy-right">
+				</ul>
+				
+				<h3 style="color:#ABABAB">connect us on social media</h3>
+				
+ </div>
+ <hr>
+ <div class="row">
+
+<div class="col-lg-4 col-md-4 col-sm-12 ">
+
+          <p><i class="fa fa-home" aria-hidden="true"></i>12K Street , 45 Building Road Canada.</p>
+</div>
+
+<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 col-lg-offset-1 col-md-offset-1">
+
+         <p><i class="fa fa-phone" aria-hidden="true"></i>+1234 758 839 , +1273 748 730</p>	
+</div>
+
+<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 col-lg-offset-1 col-md-offset-1">
+
+          <p><a style="color:#ABABAB" href="mailto:info@example.com"><i class="fa fa-envelope-o" aria-hidden="true"></i>info@example1.com</a></p>
+</div>
+</div>
+<div  style="margin-top: 2em; text-align:center;">
+<hr>
 			<p> &copy; 2016 Big store. All Rights Reserved</a></p>
 		</div>
-	</div>
-</div>
-</body>
 
-</html>
+</body>
+<html>
+
+
