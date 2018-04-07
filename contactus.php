31cbed2d06325de8ef7011b7749922318113f545
shephali97 <36363605+shephali97@users.noreply.github.com>
2018-04-07 12:16:34 +0530

    Add files via upload

---
+++ contactus.php
@@ -0,0 +1,152 @@
+<html>
+<head>
+
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ 
+ 
+<title>Big store a Ecommerce Online Shopping Category Flat Bootstrap Responsive Website Template| Home :: w3layouts</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta property="og:title" content="Vide" />
+<meta name="keywords" content="Big store Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<!-- js -->
+   <script src="js/jquery-1.11.1.min.js"></script>
+<!-- //js -->
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Noto+Sans:400,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+
+
+
+<style>
+.deco{
+
+padding:5px;
+margin:auto;
+height:auto;
+width:250px
+
+
+}
+button:hover{
+background-color:grey
+ width: 100%;
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;}
+	
+	input:hover{
+background-color:grey
+ width: 100%;
+    background-color: #4CAF50;
+    color: black;
+    padding: 10px 15px;
+    
+    border: none;
+    border-radius: 3px;
+    cursor: pointer;}
+	
+		textarea:hover{
+background-color:grey
+ width: 100%;
+    background-color: #4CAF50;
+    color: black;
+    padding: 10px 15px;
+    margin: 5px 0;
+    border: none;
+    border-radius: 3px;
+    cursor: pointer;}
+	
+	
+
+
+</style>
+</head>
+<body >
+<div class=""> 
+
+<div class="row">
+
+<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12" >
+<img class="img-responsive img"  src="images/contact-us.png" class="img-responsive" alt="Cinque Terre" style="width:150% ; height:auto;margin-top:115px"  >
+ 
+</div>
+
+
+<div class=" col-lg-6 col-md-6  col-sm-10 col-xs-10 col-lg-offset-2 col-md-offset-2  col-xs-offset-2 col-sm-offset-2"  >
+
+
+<h2 style="font-family:verdana;">Drop A Line</h2>
+<i class="glyphicon glyphicon-envelope"  style="font-size:48px"> </i>
+<p style="font-family:verdana;">Don't hessitate to contact us!!</p>
+<br>
+<p class="mbr-text align-left mbr-fonts-style display-7">
+                            Phone: +1 (0) 000 0000 001 <br>
+                            Email: youremail@mail.com
+                        </p>
+<form   method="post" target=_blank >
+Name:<br>
+<input  type="text" name="name" placeholder="Name:" >
+<br>
+<br>
+Number:<br>
+  <input  type="number" name="number" placeholder="Number">
+  <br>
+  <br>
+  Email:<br>
+   <input type="text" name="emailid" placeholder="Email">
+  <br><br>
+  Message<br>
+  <textarea name="message" rows="5" cols="30" placeholder="Enter Message"></textarea >
+  <br><br>
+  <button type="button" class="btn btn-info btn-lg ">send message</button>
+</form>
+
+</div>
+
+
+</div>
+</div>
+<?php
+include('footer.php');
+?>
+
+
+
+
+
+ 
+
+</body>
+</html>
